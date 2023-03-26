ef96f1afa0db23a85d7f7c7b8d76a09b04f8e805
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-03-26 22:48:02 +0100

    rename repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import { HomePage, AboutMe, ContactMe } from "./screens/index";
-import { Link, Routes, Route } from "react-router-dom";
+import { NavBar } from "./components/index";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/aboutMe">Sobre mi</Link>
-          </li>
-          <li>
-            <Link to="/contactMe">Mi contacto</Link>
-          </li>
-        </ul>
-      </nav>
-
+    <div>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/aboutMe" element={<AboutMe />} />
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,3 +1,24 @@
-const NavBar = () => {};
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <p>
+        <Link to="/">Juan Rifel</Link>
+      </p>
+      <ul>
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/aboutMe">Sobre mi</Link>
+        </li>
+        <li>
+          <Link to="/contactMe">Mi contacto</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
